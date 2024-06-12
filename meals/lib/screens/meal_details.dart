77c4e7898cff0cdf5fb83fa780a36c95f7047c8e
iamsabbir1 <ce21001@mbstu.ac.bd>
2024-06-11 19:48:04 -0800

    07:47pm - 11-06-2024

---
+++ meals/lib/screens/meal_details.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+
+class MealDetailsScreen extends StatelessWidget {
+  const MealDetailsScreen({super.key, required this.meal});
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(meal.title),
+        ),
+        body: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              width: double.infinity,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+            const SizedBox(height: 14),
+            for (final ingredient in meal.ingredients)
+              Text(
+                ingredient,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+              ),
+            const SizedBox(height: 24),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+            const SizedBox(height: 14),
+            for (final step in meal.steps)
+              Text(
+                step,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+              ),
+          ],
+        ));
+  }
+}
---
+++ meals/lib/screens/tab_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/meals.dart';
+
+class TabScreen extends StatefulWidget {
+  const TabScreen({super.key});
+
+  @override
+  State<TabScreen> createState() {
+    return _TabScreenState();
+  }
+
+}
+
+class _TabScreenState extends State<TabScreen> {
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Map<String, Object>> _pages = [
+      {
+        'page': MealsScreen(),
+        'title': 'Categories',
+      },
+      {
+        'page': const FavoritesScreen(),
+        'title': 'Favorites',
+      },
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title'] as String),
+      ),
+      body: _pages[_selectedPageIndex]['page'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).colorScheme.secondary,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+    );
+  }
+}
